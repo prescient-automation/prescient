172c5f772dc62588693fa1eed33edbc0712d26fc
Prescient Automation <60386578+prescient-automation@users.noreply.github.com>
2020-03-02 04:24:31 +0530

    Update webhook_1.py

--- webhook_1.py
+++ webhook_1.py
@@ -104,9 +104,3 @@ def static_reply():
     return r
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print("Starting app on port %d" % port)
-
-    app.run(debug=True, port=port, host='0.0.0.0')
